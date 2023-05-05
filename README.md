4d8b5ea62fe8d817dd20719cf48cfe76d08b5379
Gwenael Pluchon <accounts@gwenp.fr>
2023-05-05 13:56:59 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # testcafe-reporter-json-custom
+[![Build Status](https://travis-ci.org/gwenaelp/testcafe-reporter-json-custom.svg)](https://travis-ci.org/gwenaelp/testcafe-reporter-json-custom)
+
+This is the **json-custom** reporter plugin for [TestCafe](http://devexpress.github.io/testcafe).
+
+<p align="center">
+    <img src="https://raw.github.com/gwenaelp/testcafe-reporter-json-custom/master/media/preview.png" alt="preview" />
+</p>
+
+## Install
+
+```
+npm install testcafe-reporter-json-custom
+```
+
+## Usage
+
+When you run tests from the command line, specify the reporter name by using the `--reporter` option:
+
+```
+testcafe chrome 'path/to/test/file.js' --reporter json-custom
+```
+
+
+When you use API, pass the reporter name to the `reporter()` method:
+
+```js
+testCafe
+    .createRunner()
+    .src('path/to/test/file.js')
+    .browsers('chrome')
+    .reporter('json-custom') // <-
+    .run();
+```
+
+## Author
+Gwenael Pluchon 
---
+++ src/index.js
@@ -0,0 +1,69 @@
+module.exports = function (source, arg2) {
+    return {
+        noColors:       true,
+        currentFixture: null,
+
+        report: {
+            startTime:  null,
+            endTime:    null,
+            userAgents: null,
+            passed:     0,
+            total:      0,
+            skipped:    0,
+            fixtures:   [],
+            warnings:   []
+        },
+
+        reportTaskStart (startTime, userAgents, testCount) {
+            this.report.startTime  = startTime;
+            this.report.userAgents = userAgents;
+            this.report.total      = testCount;
+        },
+
+        reportFixtureStart (name, path, meta) {
+            if(projectRoot) {
+                path = path.replace(projectRoot, '');
+            }
+
+            this.currentFixture = { name, path, meta, tests: [] };
+            this.report.fixtures.push(this.currentFixture);
+        },
+
+        reportTestDone (name, testRunInfo, meta) {
+            var errs = testRunInfo.errs.map(err => this.formatError(err));
+            
+            if (testRunInfo.skipped)
+                this.report.skipped++;
+
+            let videos;
+            if (testRunInfo.videos) {
+                videos = testRunInfo.videos.map(v => {
+                    if(projectRoot && v.videoPath) {
+                        return v.videoPath.replace(projectRoot, '');
+                    }
+                    return v.videoPath;
+                });
+            }
+
+            this.currentFixture.tests.push({
+                name: name,
+                meta: meta,
+                errs: errs,
+
+                durationMs:     testRunInfo.durationMs,
+                unstable:       testRunInfo.unstable,
+                screenshotPath: testRunInfo.screenshotPath,
+                skipped:        testRunInfo.skipped,
+                videos,
+            });
+        },
+
+        reportTaskDone (endTime, passed, warnings) {
+            this.report.passed   = passed;
+            this.report.endTime  = endTime;
+            this.report.warnings = warnings;
+
+            this.write(JSON.stringify(this.report, null, 2));
+        }
+    };
+}
